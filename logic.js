562166feabdde4b7ef4f7c2b393554ac9226554f
Lexi <you@example.com>
2017-07-17 10:32:48 -0500

    prettify code

--- logic.js
+++ logic.js
@@ -25,129 +25,129 @@ function startProgram() {
 };
 
 var CreateNew = function() {
+    inquirer.prompt([{
+        name: "type",
+        message: "Would you like to create a basic or a cloze flashcard?",
+        type: "list",
+        choices: [{
+            name: "basic"
+        }, {
+            name: "cloze"
+
+        }]
+    }]).then(function(choices) {
+        if (choices.type === "basic") {
+            NewBasic();
+        } else if (choices.type === "cloze") {
+            NewCloze();
+        }
+    })
+};
+
+
+var NewBasic = function() {
+    inquirer.prompt([{
+        name: "front",
+        message: "What is the question you've chosen for this card?",
+        validate: function(input) {
+            if (input === "") {
+                console.log('Please provide a question');
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }, {
+        name: "back",
+        message: "What is the answer to this question?",
+        validate: function(input) {
+            if (input === "") {
+                console.log("Please provide an answer");
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }]).then(function(answer) {
+        var basicCard = new BasicCard(answer.front, answer.back);
+        flashcards.push(basicCard);
+        console.log("Flashcard created!");
+        startProgram();
+    });
+};
+
+var NewCloze = function() {
+    inquirer.prompt([{
+        name: "information",
+        message: "What is the full text for this card?",
+        validate: function(input) {
+            if (input === "") {
+                console.log("Please provide text for this card.");
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }]).then(function(answer) {
+        fullText = answer.information;
         inquirer.prompt([{
-                        name: "type",
-                        message: "Would you like to create a basic or a cloze flashcard?",
-                        type: "list",
-                        choices: [{
-                        	name: "basic"
-                        }, {
-                        	name: "cloze"
-                        
-                    }]
-                       }]).then(function(choices) {
-                            if (choices.type === "basic") {
-                                NewBasic();
-                            } else if (choices.type === "cloze") {
-                                NewCloze();
-                            }
-                        })
-                    };
-                
+            name: "hidden",
+            message: "What is the cloze portion of this flashcard?",
+            validate: function(input) {
+                if (input === "") {
+                    console.log("Please input the cloze portion of this flashcard.");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        }]).then(function(answers) {
+            console.log(answers);
+            var hidden = answers.hidden;
+            if (fullText.includes(hidden)) {
+                var clozeCard = new ClozeCard(fullText, hidden);
+                flashcards.push(clozeCard);
+                console.log("Flashcard created!")
+                startProgram();
+            } else {
+                console.log("The cloze portion you provided is not in the full text. Please try again.");
+                startProgram();
+            }
 
-                    var NewBasic = function() {
-                        inquirer.prompt([{
-                            name: "front",
-                            message: "What is the question you've chosen for this card?",
-                            validate: function(input) {
-                                if (input === "") {
-                                    console.log('Please provide a question');
-                                    return false;
-                                } else {
-                                    return true;
-                                }
-                            }
-                        }, {
-                            name: "back",
-                            message: "What is the answer to this question?",
-                            validate: function(input) {
-                                if (input === "") {
-                                    console.log("Please provide an answer");
-                                    return false;
-                                } else {
-                                    return true;
-                                }
-                            }
-                        }]).then(function(answer) {
-                            var basicCard = new BasicCard(answer.front, answer.back);
-                            flashcards.push(basicCard);
-                            console.log("Flashcard created!");
-                            startProgram();
-                        });
-                    };
+        });
+    });
+}
 
-                    var NewCloze = function() {
-                        inquirer.prompt([{
-                            name: "information",
-                            message: "What is the full text for this card?",
-                            validate: function(input) {
-                                if (input === "") {
-                                    console.log("Please provide text for this card.");
-                                    return false;
-                                } else {
-                                    return true;
-                                }
-                            }
-                        }]).then(function(answer) {
-                            fullText = answer.information;
-                            inquirer.prompt([{
-                                name: "hidden",
-                                message: "What is the cloze portion of this flashcard?",
-                                validate: function(input) {
-                                    if (input === "") {
-                                        console.log("Please input the cloze portion of this flashcard.");
-                                        return false;
-                                    } else {
-                                        return true;
-                                    }
-                                }
-                            }]).then(function(answers) {
-                                console.log(answers);
-                                var hidden = answers.hidden;
-                                if (fullText.includes(hidden)) {
-                                    var clozeCard = new ClozeCard(fullText, hidden);
-                                    flashcards.push(clozeCard);
-                                    console.log("Flashcard created!")
-                                    startProgram();
-                                } else {
-                                    console.log("The cloze portion you provided is not in the full text. Please try again.");
-                                    startProgram();
-                                }
+var ReviewCards = function(index) {
+    card = flashcards[index];
 
-                            });
-                        });
-                    }
+    var questionText;
+    var correctResponse;
 
-                    var ReviewCards = function(index) {
-                        card = flashcards[index];
-                        
-                        var questionText;
-                        var correctResponse;
+    if (card.type === "basic") {
+        questionText = card.front;
+        correctResponse = card.back;
+    } else if (card.type === "cloze") {
+        questionText = card.clozeText;
+        correctResponse = card.cloze;
+    }
+    inquirer.prompt([{
+        name: "response",
+        message: questionText
+    }]).then(function(answer) {
+        if (answer.response === correctResponse) {
+            console.log("That's correct!")
+            if (index < flashcards.length - 1) {
+                ReviewCards(index + 1);
+            }
+        } else {
+            console.log("Sorry, that is incorrect!");
+            if (index < flashcards.length - 1) {
+                ReviewCards(index + 1)
+            }
+        }
 
-                        if (card.type === "basic") {
-                            questionText = card.front;
-                            correctResponse = card.back;
-                        } else if (card.type === "cloze") {
-                            questionText = card.clozeText;
-                            correctResponse = card.cloze;
-                        }
-                        inquirer.prompt([{
-                            name: "response",
-                            message: questionText
-                        }]).then(function(answer) {
-                            if (answer.response === correctResponse) {
-                                console.log("That's correct!")
-                                if (index < flashcards.length - 1) {
-                                    ReviewCards(index + 1);
-                                }
-                            } else {
-                                console.log("Sorry, that is incorrect!");
-                                if (index < flashcards.length - 1) {
-                                    ReviewCards(index + 1)
-                                }
-                            }
-
-                        });
-                        console.log("All cards reviewed!");
-                        startProgram();
-                    };
+    });
+    console.log("All cards reviewed!");
+    startProgram();
+};
